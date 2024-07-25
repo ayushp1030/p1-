c77c4b49d7e009a2922e3c03079fa33a59ea9b14
AYUSH PATEL <ayushp0848@gmail.com>
2024-07-25 18:47:13 +0530

    Add files via upload

---
+++ p1/Q = 1.cpp
@@ -0,0 +1,17 @@
+// Develop a program that converts temperature from degrees Celsius to Fahrenheit using the
+
+#include<stdio.h>
+#include<conio.h>
+
+ main()
+{
+	float Fahrenheit,Celsius;
+	
+	printf("Enter the value in Celsius : ");
+	scanf("%f",&Celsius);
+	
+	f = (Celsius*9/5)+32;
+	
+	printf("Fahrenheit : %.2f",Celsius);
+	
+}
---
+++ p1/Q = 3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+ main()
+{
+    int a, b, c;
+
+    
+    printf("Enter two angles of triangle: ");
+    scanf("%d%d", &a, &b);
+
+   
+    c = 180 - (a + b);
+
+   
+    printf("Third angle of the triangle = %d", c);
+
+    
+}
+
